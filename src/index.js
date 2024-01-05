35f19ddc67799aabefeffe2b5eedab394be4442b
ochelle <alexizochelle@gmail.com>
2024-01-05 17:36:40 +0300

    Still working on it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+    const createTaskForm = document.getElementById('create-task-form');
+    createTaskForm.addEventListener('submit', function(e){
+      e.preventDefault();
+      const taskDescription = document.getElementById('new-task-description');
+    });
+    function createTaskItem(description){
+      const tasklist = document.getElementById ('task-list');
+      const li = document.createElement('li');
+      li.textContent = description;
+      //addition of the deletion button
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.addEventListener('click', function(){
+        //removes the task item when the delete button is clicked
+        li.remove();
+      })
+      li.append(deleteButton);
+      tasklist.appendChild(li);
+    }
+    //modification of the form submit event listener to create a new task item
+    createTaskForm.addEventListener('submit', function (e){
+      e.preventDefault();
+    })
+  });
+
